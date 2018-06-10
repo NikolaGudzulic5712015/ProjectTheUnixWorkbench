e3bb99c6dded6b846e1d68cd2d43b76042cf2e9c
Nikola Gudžulić 571/2015 <nikolagu74@gmail.com>
2018-06-10 23:11:57 +0200

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+################################################################################
+#Script name	:GUESSING GAME
+#Description	:User try to match number of files.
+#Args		:
+#Author		:Nikola Gudžulić
+#Email		:nikolagu74@gmail.com
+################################################################################
+
+numberOfFiles=$(ls -l | wc -l)
+
+
+Welcome()
+{
+	echo "Welcome to the GuessingGame";
+	echo "How many files are in current directory?";
+}
+
+Match()
+{
+	echo "Great! You are lucky guy!";
+	echo "That's correct!";
+}
+
+Welcome
+
+
+
+
+
+until [[ $input == $numberOfFiles ]]; 
+do
+read input	
+
+	if [[ $input -lt $numberOfFiles ]]; then
+		echo "The guess is too low"
+		echo "Enter the new guess:"
+		
+	elif [[ $input -gt $numberOfFiles ]]; then
+		echo "The guess is too high"
+		echo "Enter the new guess:"
+		
+	fi
+
+done
+Match
+
+
+
+
+	
+
+
